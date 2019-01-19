c766699962465be07b650d70df0bf9e42dfb658b
Cyril FAISANDIER <cyril.faisandier@gmail.com>
2019-01-19 10:48:25 +0100

    [backend/feature_ApiUser] add swagger and list user

---
+++ backend/src/main/java/com/groupir/backend/service/ServiceUser.java
@@ -0,0 +1,38 @@
+package com.groupir.backend.service;
+
+import com.groupir.backend.model.User;
+import com.groupir.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ServiceUser {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAllUser(){
+        return (List<User>) userRepository.findAll();
+    }
+    /*
+    public HttpStatus add(User newUser) {
+        userRepository.save(newUser);
+        return HttpStatus.OK;
+    }
+
+    public HttpStatus delete(int idUser) {
+        userRepository.deleteById(idUser);
+        return HttpStatus.OK;
+    }
+
+    public HttpStatus update( User updateUser) {
+        userRepository.save(updateUser);
+        return HttpStatus.OK;
+    }*/
+}
