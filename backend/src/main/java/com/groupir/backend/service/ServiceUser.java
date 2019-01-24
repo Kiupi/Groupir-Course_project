a5458bf02040b6f91289882d63c979e15181c223
Cyril FAISANDIER <cyril.faisandier@gmail.com>
2019-01-24 16:43:42 +0100

    [backend/feature_ApiUser] add javadoc

--- backend/src/main/java/com/groupir/backend/service/ServiceUser.java
+++ backend/src/main/java/com/groupir/backend/service/ServiceUser.java
@@ -3,10 +3,8 @@ package com.groupir.backend.service;
 import com.groupir.backend.model.User;
 import com.groupir.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -17,20 +15,44 @@ public class ServiceUser {
     @Autowired
     private UserRepository userRepository;
 
-
+    /**
+     *  find all user from database
+     * @return list of all user
+     */
     public List<User> findAllUser(){
         return (List<User>) userRepository.findAll();
     }
 
+    /**
+     *  Add a user in database
+     * @param newUser is a user
+     */
     public void add(User newUser) {
         userRepository.save(newUser);
     }
 
+    /**
+     *  Delete a user in database
+     * @param idUser
+     */
     public void delete(int idUser) {
         userRepository.deleteById(idUser);
     }
 
+    /**
+     *  update a user drom database
+     * @param updateUser is a user
+     */
     public void update( User updateUser) {
         userRepository.save(updateUser);
+    }
+
+    /**
+     *  check the id of user in database
+     * @param idUser is id of user
+     * @return false if isn't present in database else false
+     */
+    public boolean findById(int idUser) {
+        return userRepository.findById(idUser).isPresent();
     }
 }
