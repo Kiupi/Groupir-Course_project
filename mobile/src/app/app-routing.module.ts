af9ff73b06973cefccc6e3668802db322eb52423
jebouin <jebouin@gmail.com>
2019-01-28 10:00:37 +0100

    Add products page

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule' },
-
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule' },
+  { path: 'products', loadChildren: './products/products.module#ProductsPageModule' },
 ];
 
 @NgModule({
