9f99b443683beeaba646aab126ae6e00695d081c
Théo Basty <theo.basty@gmail.com>
2019-01-28 10:35:38 +0100

    [backend]Supplier part. IN PROGRESS

--- backend/src/main/java/com/groupir/backend/model/Order.java
+++ backend/src/main/java/com/groupir/backend/model/Order.java
@@ -1,5 +1,6 @@
 package com.groupir.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -18,10 +19,12 @@ public class Order {
 
     @ManyToOne
     @JoinColumn(nullable = false)
+    @JsonIgnore
     private User user;
 
     @ManyToOne
     @JoinColumn(nullable = false)
+    @JsonIgnore
     private PaymentMethod paymentMethod;
 
     @ManyToOne
--- backend/src/main/java/com/groupir/backend/service/ServiceUser.java
+++ backend/src/main/java/com/groupir/backend/service/ServiceUser.java
@@ -1,12 +1,15 @@
 package com.groupir.backend.service;
 
+import com.groupir.backend.exceptions.ExceptionNoUserForPrincipal;
 import com.groupir.backend.model.User;
 import com.groupir.backend.repository.UserRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
