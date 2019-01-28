9efd7289e4b4cb805817c5bd15d1876932535918
Kiupi <quent407@gmail.com>
2019-01-28 11:15:48 +0100

    [Frontend/loggin-page] loggin page v0.1 & forgot page v0.0

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'loggin', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule' },
   { path: 'loggin', loadChildren: './loggin/loggin.module#LogginPageModule' },
+  { path: 'forgot-page', loadChildren: './forgot-page/forgot-page.module#ForgotPagePageModule' },
   { path: 'products', loadChildren: './products/products.module#ProductsPageModule' },
 ];
 
