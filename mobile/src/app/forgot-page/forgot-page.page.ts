f25bab4c0b666da94d768cb1050e1bd5f58cda82
Kiupi <quent407@gmail.com>
2019-01-28 11:15:48 +0100

    [Frontend/loggin-page] loggin page v0.1 & forgot page v0.0

---
+++ mobile/src/app/forgot-page/forgot-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-page',
+  templateUrl: './forgot-page.page.html',
+  styleUrls: ['./forgot-page.page.scss'],
+})
+export class ForgotPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
