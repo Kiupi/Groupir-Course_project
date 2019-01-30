745aa4fd1525dce06edef3976c4eab8db1a6c525
Kiupi <quent407@gmail.com>
2019-01-30 11:42:55 +0100

    [Frontend/loggin-page] edit name package login

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -8,14 +11,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent{
+
+  title : 'app';
+  restItems: any;
+
+  //TODO : change API URL
+  restItemsUrl: 'https://public-api.wordpress.com/rest/v1.1/sites/vocon-it.com/posts';
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private http: HttpClient
   ) {
     this.initializeApp();
   }
+
+  ngOnInit(){
+  }
+
 
   initializeApp() {
     this.platform.ready().then(() => {
--- mobile/src/app/login/login.page.spec.ts
+++ mobile/src/app/login/login.page.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LogginPage } from './loggin.page';
+import { LoginPage } from './login.page';
 
-describe('LogginPage', () => {
-  let component: LogginPage;
-  let fixture: ComponentFixture<LogginPage>;
+describe('LoginPage', () => {
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LogginPage ],
+      declarations: [ LoginPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LogginPage);
+    fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
