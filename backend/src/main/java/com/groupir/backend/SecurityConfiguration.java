3ba5aabbf9884d273a27b74623dc8605d783f30d
Théo Basty <theo.basty@gmail.com>
2019-02-04 09:37:46 +0100

    [backend]Adding authentication

--- backend/src/main/java/com/groupir/backend/SecurityConfiguration.java
+++ backend/src/main/java/com/groupir/backend/SecurityConfiguration.java
@@ -1,39 +1,55 @@
 package com.groupir.backend;
 
+import com.groupir.backend.security.JwtConfigurer;
+import com.groupir.backend.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Bean
     @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/user/login").permitAll()
+                .antMatchers("/api/user/list").hasAuthority("ADMIN")
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring()
-                .antMatchers("/**");
+//        web
+//                .ignoring()
+//                .antMatchers("/**")
+        ;
     }
 
 }
