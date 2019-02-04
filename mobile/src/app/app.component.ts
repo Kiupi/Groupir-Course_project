938f4c19e0fb276ea65b0f8074ebe900c425a7ef
Kiupi <quent407@gmail.com>
2019-02-04 11:41:18 +0100

    [Frontend/loggin-page] fin login ---Non testé avec l'api---

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -1,41 +1,40 @@
-import { Component,OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
 })
-export class AppComponent{
+export class AppComponent {
 
-  title : 'app';
-  restItems: any;
+    title: 'app';
+    restItems: any;
 
-  //TODO : change API URL
-  restItemsUrl: 'https://public-api.wordpress.com/rest/v1.1/sites/vocon-it.com/posts';
+    //TODO : change API URL
+    restItemsUrl: 'https://public-api.wordpress.com/rest/v1.1/sites/vocon-it.com/posts';
 
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private http: HttpClient
-  ) {
-    this.initializeApp();
-  }
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private http: HttpClient
+    ) {
+        this.initializeApp();
+    }
 
-  ngOnInit(){
-  }
+    ngOnInit() {
+    }
 
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 }
--- mobile/src/app/login/login.page.ts
+++ mobile/src/app/login/login.page.ts
@@ -1,19 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {LoadingController, NavController, ToastController} from '@ionic/angular';
+import {AuthService} from '../auth.service';
+import {finalize} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-loggin',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-loggin',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  constructor(public router: Router) { }
+    constructor(public router: Router,
+                private readonly navCtrl: NavController,
+                private readonly loadingCtrl: LoadingController,
+                private readonly authService: AuthService,
+                private readonly toastCtrl: ToastController) {
+    }
 
-  ngOnInit() {
-  }
+    async login(username: any, password: any) {
+        const loading = await this.loadingCtrl.create({
+            spinner: 'bubbles',
+            message: 'Logging in ...'
+        });
 
-  navigate(){
-    this.router.navigate(['/forgot-page'])
-  }
+        loading.present();
+
+        this.authService
+            .login(username, password)
+            .pipe(finalize(() => loading.dismiss()))
+            .subscribe(
+                _ => {
+                    this.navCtrl.navigateRoot(['home'], {replaceUrl: true});
+                },
+                err => this.handleError(err));
+    }
+
+    async handleError(error: any) {
+        let message: string;
+        if (error.status && error.status === 401) {
+            message = 'Login failed';
+        } else {
+            message = 'Unexpected error: ${error.statusText}';
+        }
+
+        const toast = await this.toastCtrl.create({
+            message,
+            duration: 5000,
+            position: 'bottom'
+        });
+
+        toast.present();
+    }
+
+    ngOnInit() {
+    }
+
+    navigate() {
+        this.router.navigate(['/forgot-page']);
+    }
 }
