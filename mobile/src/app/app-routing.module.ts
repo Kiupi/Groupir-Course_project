c28f849e5dc23ce45202e684b5bd2c5491e54273
Kiupi <quent407@gmail.com>
2019-02-04 10:07:24 +0100

    [Frontend/loggin-page] authentification file

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AuthGuard} from './auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'forgot-page', loadChildren: './forgot-page/forgot-page.module#ForgotPagePageModule' },
-  { path: 'products', loadChildren: './products/products.module#ProductsPageModule' },
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '**', redirectTo: 'login'},
+    {path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard]},
+    {path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule'},
+    {path: 'login', loadChildren: './login/login.module#LoginPageModule'},
+    {path: 'forgot-page', loadChildren: './forgot-page/forgot-page.module#ForgotPagePageModule'},
+    {path: 'products', loadChildren: './products/products.module#ProductsPageModule'},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
