32d421af2fbfbbcf3f499366040da71e8eccf668
Cyril FAISANDIER <cyril.faisandier@gmail.com>
2019-02-06 13:54:42 +0100

    [backend/feature_ApiUser] add purchases history (in progress)

--- backend/src/main/java/com/groupir/backend/dao/AuthenticationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.groupir.backend.dao;
-
-import lombok.Data;
-
-@Data
-public class AuthenticationRequest {
-    String email;
-    String password;
-}
--- backend/src/main/java/com/groupir/backend/model/Order.java
+++ backend/src/main/java/com/groupir/backend/model/Order.java
@@ -30,4 +30,6 @@ public class Order {
     @ManyToOne
     @JoinColumn(nullable = false)
     private Address address;
+
+
 }
