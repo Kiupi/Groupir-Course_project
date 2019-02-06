66bf74cec76271d8ae99a5ec3f426076060b8140
jebouin <jebouin@gmail.com>
2019-02-06 10:51:37 +0100

    Add categories menu

--- mobile/src/app/products/products.page.ts
+++ mobile/src/app/products/products.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 import * as moment from 'moment';
 import 'moment/locale/pt-br';
 import { MenuController } from '@ionic/angular';
@@ -11,14 +12,15 @@ import { MenuController } from '@ionic/angular';
 export class ProductsPage implements OnInit {
   products: any;
   categories: any;
+  category: String;
 
-  constructor(private menu: MenuController) {
+  constructor(private menu: MenuController, private router: Router, private route: ActivatedRoute) {
     moment.locale('FR-fr');
     let page = this;
     this.products = [
-      {quantity: 105, name: "Dot Product", image: "https://www.cmath.fr/1ere/produitscalaire/1images8/dessin5.gif", description: "In mathematics, the dot product or scalar product is an algebraic operation that takes two equal-length sequences of numbers (usually coordinate vectors) and returns a single number."},
-      {quantity: 84, name: "Computer Keyboard", image: "https://cdn.wccftech.com/wp-content/uploads/2018/11/Wooting-Seasonic-Partnership.jpg", description: "In computing, a computer keyboard is a typewriter-style device which uses an arrangement of buttons or keys to act as mechanical levers or electronic switches. Following the decline of punch cards and paper tape, interaction via teleprinter-style keyboards became the main input method for computers."},
-      {quantity: 302, name: "Spring", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPnpKaQGLvgftLaJhf-P2FSK8xlMpcy4RyslOPo5DtozY6r3AFsA", description: "A spring is an elastic object that stores mechanical energy. Springs are typically made of spring steel. There are many spring designs. In everyday use, the term often refers to coil springs."},
+      {quantity: 105, category: 1, name: "Dot Product", image: "https://www.cmath.fr/1ere/produitscalaire/1images8/dessin5.gif", description: "In mathematics, the dot product or scalar product is an algebraic operation that takes two equal-length sequences of numbers (usually coordinate vectors) and returns a single number."},
+      {quantity: 84, category: 2, name: "Computer Keyboard", image: "https://cdn.wccftech.com/wp-content/uploads/2018/11/Wooting-Seasonic-Partnership.jpg", description: "In computing, a computer keyboard is a typewriter-style device which uses an arrangement of buttons or keys to act as mechanical levers or electronic switches. Following the decline of punch cards and paper tape, interaction via teleprinter-style keyboards became the main input method for computers."},
+      {quantity: 302, category: 3, name: "Spring", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPnpKaQGLvgftLaJhf-P2FSK8xlMpcy4RyslOPo5DtozY6r3AFsA", description: "A spring is an elastic object that stores mechanical energy. Springs are typically made of spring steel. There are many spring designs. In everyday use, the term often refers to coil springs."},
     ];
     this.products.forEach(function(product) {
       product.endDate = new Date();
@@ -26,10 +28,7 @@ export class ProductsPage implements OnInit {
       product.endDate.setSeconds(product.endDate.getSeconds() + seconds);
       page.updateRemainingTime(product);
     });
-    this.categories = [];
-    for(let i = 1; i <= 30; i++) {
-      this.categories.push({name: "Categorie " + i});
-    }
+    this.categories = [{name: "All"}, {name: "Categorie 1", id: 1}, {name: "Categorie 2", id: 2}, {name: "Categorie 3", id: 3}];
   }
 
   openCategoriesMenu() {
@@ -52,7 +51,7 @@ export class ProductsPage implements OnInit {
   loadData(event) {
     setTimeout(() => {
       for(let i = 0; i < 3; i++) {
-        this.products.push(this.products[i])
+        this.products.push(this.products[Math.floor(Math.random() * this.products.length)]);
       }
       event.target.complete();
       if (this.products.length == 100) {
@@ -61,13 +60,22 @@ export class ProductsPage implements OnInit {
     }, Math.random() * 1000 + 100);
   }
 
-  onCategoryClick(event, category) {
-    console.log(category);
+  goToCategory(category) {
+      let url = "/products";
+      if(category != null) {
+          url += "?category=" + category;
+      }
+      this.router.navigateByUrl(url);
+      this.category = category;
+  }
+
+  onCategoryClick(event, categoryId) {
     this.menu.close('categories');
+    this.goToCategory(categoryId);
   }
 
   ngOnInit() {
     this.timerTick();
+    this.category = this.route.snapshot.queryParamMap.get("category");
   }
-
 }
