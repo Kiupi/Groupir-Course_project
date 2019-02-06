440e317f7defe5537929098a53a0e6313f76da6b
Théo Basty <theo.basty@gmail.com>
2019-02-06 12:54:11 +0100

    [backend] Beginning unit testing of security in user controller

--- backend/src/test/java/com/groupir/backend/controller/UserRestControllerTest.java
+++ backend/src/test/java/com/groupir/backend/controller/UserRestControllerTest.java
@@ -8,6 +8,10 @@ import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import jdk.nashorn.internal.runtime.options.Option;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,6 +30,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
@@ -37,6 +44,12 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -413,6 +426,63 @@ class UserRestControllerTest {
 
     @Nested
     class SupplierAccess {
+
+    }
+
+    @Nested
+    class AnonymousAccess {
+        // /api/user/add
+        @WithAnonymousUser
+        @Test
+        void Anonymous_shouldBeAbleToRegisterUser() throws Exception {
+            given(serviceUser.add(any(User.class))).willAnswer(invocation -> {
+                User newUser = invocation.getArgument(0);
+                newUser.setUserId(7);
+                return newUser;
+            });
+
+            String newUserJSon = "  {\n" +
+                    "    \"lastName\": \"User\",\n" +
+                    "    \"firstName\": \"New\",\n" +
+                    "    \"birthDate\": null,\n" +
+                    "    \"role\": {\n" +
+                    "      \"roleId\": 2\n" +
+                    "    },\n" +
+                    "    \"email\": \"new.user@test.test\",\n" +
+                    "    \"defaultAddress\": null,\n" +
+                    "    \"password\": \"test\"\n" +
+                    "  }";
+
+            MvcResult result = mockMvc.perform(post("/api/user/add")
+                    .content(newUserJSon)
+                    .contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(status().isOk()).andReturn();
+        }
+
+        @WithAnonymousUser
+        @Test
+        void Anonymous_shouldNotBeAbleToRegisterAdmin(){
+            fail();
+        }
+
+        @WithAnonymousUser
+        @Test
+        void Anonymous_shouldNotBeAbleToRegisterSupplier(){
+            fail();
+        }
+
+        // /api/user/
+    }
+    @Nested
+    class AdminAccess {
+
+    }
+    @Nested
+    class UserAccess {
+
+    }
+    @Nested
+    class SupplierAccess{
 
     }
 }
