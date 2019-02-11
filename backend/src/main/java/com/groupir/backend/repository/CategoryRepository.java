451c35a236f7c76f38d1fae557b00c8e6552c40f
BaptisteWolff <baptiste.wolff@telecom-st-etienne.fr>
2019-02-11 16:52:25 +0100

    Category : get, add delete, update and list only ids

--- backend/src/main/java/com/groupir/backend/repository/CategoryRepository.java
+++ backend/src/main/java/com/groupir/backend/repository/CategoryRepository.java
@@ -1,7 +1,15 @@
 package com.groupir.backend.repository;
 
 import com.groupir.backend.model.Category;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
+    @Query(value = "select category_id from category", nativeQuery = true)
+    List<Integer> getAllIds();
 }
