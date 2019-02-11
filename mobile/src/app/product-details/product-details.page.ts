f2d0b6f2937e6338adf0bafbd91ee39f5fc65f36
jebouin <jebouin@gmail.com>
2019-02-11 17:52:44 +0100

    Add basic product details page layout with option selection

--- mobile/src/app/product-details/product-details.page.ts
+++ mobile/src/app/product-details/product-details.page.ts
@@ -8,15 +8,34 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ProductDetailsPage implements OnInit {
   productId: Number;
+  product: any;
+  selectedOptionId: Number;
 
   constructor(private route: ActivatedRoute) {
     let page = this;
+    this.selectedOptionId = 0;
     this.route.queryParams.subscribe(params => {
       page.productId = params["id"];
+      page.product = {
+        id: page.productId,
+        name: "Sun Shield",
+        description: "vroum vroum vroum vroum vroum vroum vroum vroum vroum vroum vroum vroum vroum",
+        image: "https://ae01.alicdn.com/kf/HTB1YxCwSVXXXXXWXXXXq6xXFXXXQ/Car-Windshield-Sunshades-Window-Sun-Shield-Visor-Silver-Car-Shade-Sun-Protection-Size-92-cm-142.jpg_640x640.jpg",
+        options: [{
+          id: 0,
+          name: "92 cm"
+        }, {
+          id: 1,
+          name: "104 cm"
+        }, {
+          id: 2,
+          name: "116 cm"
+        }]
+      }
     });
   }
 
   ngOnInit() {
-  }
 
+  }
 }
