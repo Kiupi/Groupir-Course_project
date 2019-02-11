c25fc0ed7bf64a02e44eab48bc88124d889728f0
Théo Basty <theo.basty@gmail.com>
2019-02-11 14:17:02 +0100

    [backend] cleaning rebase

--- backend/src/test/java/com/groupir/backend/controller/UserRestControllerTest.java
+++ backend/src/test/java/com/groupir/backend/controller/UserRestControllerTest.java
@@ -1,6 +1,5 @@
 package com.groupir.backend.controller;
 
-import com.groupir.backend.exceptions.GlobalControllerExceptionHandler;
 import com.groupir.backend.model.Role;
 import com.groupir.backend.model.User;
 import com.groupir.backend.repository.RoleRepository;
@@ -8,21 +7,6 @@ import com.groupir.backend.repository.UserRepository;
 import com.groupir.backend.service.ServiceOrderItem;
 import com.groupir.backend.service.ServiceUser;
 import com.groupir.backend.service.ServiceUserOrders;
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import jdk.nashorn.internal.runtime.options.Option;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -35,51 +19,21 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @EnableWebMvc
@@ -717,120 +671,6 @@ class UserRestControllerTest {
                     .contentType(MediaType.APPLICATION_JSON_UTF8))
                     .andExpect(status().isForbidden()).andReturn();
         }
-    }
-
-    @Nested
-    class AnonymousAccess {
-        // /api/user/add
-        @WithAnonymousUser
-        @Test
-        void Anonymous_shouldBeAbleToRegisterUser() throws Exception {
-            given(serviceUser.add(any(User.class))).willAnswer(invocation -> {
-                User newUser = invocation.getArgument(0);
-                newUser.setUserId(7);
-                return newUser;
-            });
-
-            String newUserJSon = "  {\n" +
-                    "    \"lastName\": \"User\",\n" +
-                    "    \"firstName\": \"New\",\n" +
-                    "    \"birthDate\": null,\n" +
-                    "    \"role\": {\n" +
-                    "      \"roleId\": 2\n" +
-                    "    },\n" +
-                    "    \"email\": \"new.user@test.test\",\n" +
-                    "    \"defaultAddress\": null,\n" +
-                    "    \"password\": \"test\"\n" +
-                    "  }";
-
-            MvcResult result = mockMvc.perform(post("/api/user/add")
-                    .content(newUserJSon)
-                    .contentType(MediaType.APPLICATION_JSON_UTF8))
-                    .andExpect(status().isOk()).andReturn();
-        }
-
-        @WithAnonymousUser
-        @Test
-        void Anonymous_shouldNotBeAbleToRegisterAdmin(){
-            fail();
-        }
-
-        @WithAnonymousUser
-        @Test
-        void Anonymous_shouldNotBeAbleToRegisterSupplier(){
-            fail();
-        }
-
-        // /api/user/
-    }
-    @Nested
-    class AdminAccess {
-
-    }
-    @Nested
-    class UserAccess {
-
-    }
-    @Nested
-    class SupplierAccess{
-
-    }
-
-    @Nested
-    class AnonymousAccess {
-        // /api/user/add
-        @WithAnonymousUser
-        @Test
-        void Anonymous_shouldBeAbleToRegisterUser() throws Exception {
-            given(serviceUser.add(any(User.class))).willAnswer(invocation -> {
-                User newUser = invocation.getArgument(0);
-                newUser.setUserId(7);
-                return newUser;
-            });
-
-            String newUserJSon = "  {\n" +
-                    "    \"lastName\": \"User\",\n" +
-                    "    \"firstName\": \"New\",\n" +
-                    "    \"birthDate\": null,\n" +
-                    "    \"role\": {\n" +
-                    "      \"roleId\": 2\n" +
-                    "    },\n" +
-                    "    \"email\": \"new.user@test.test\",\n" +
-                    "    \"defaultAddress\": null,\n" +
-                    "    \"password\": \"test\"\n" +
-                    "  }";
-
-            MvcResult result = mockMvc.perform(post("/api/user/add")
-                    .content(newUserJSon)
-                    .contentType(MediaType.APPLICATION_JSON_UTF8))
-                    .andExpect(status().isOk()).andReturn();
-        }
-
-        @WithAnonymousUser
-        @Test
-        void Anonymous_shouldNotBeAbleToRegisterAdmin(){
-            fail();
-        }
-
-        @WithAnonymousUser
-        @Test
-        void Anonymous_shouldNotBeAbleToRegisterSupplier(){
-            fail();
-        }
-
-        // /api/user/
-    }
-    @Nested
-    class AdminAccess {
-
-    }
-    @Nested
-    class UserAccess {
-
-    }
-    @Nested
-    class SupplierAccess {
-
     }
 
 }
