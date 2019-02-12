66c1d02c4697fb8dfbd15705115c9fe0e2078d5b
jebouin <jebouin@gmail.com>
2019-02-12 16:01:54 +0100

    Add a basic price progress bar

--- mobile/src/app/product-details/product-details.module.ts
+++ mobile/src/app/product-details/product-details.module.ts
@@ -21,6 +21,11 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [ProductDetailsPage]
+  declarations: [
+      ProductDetailsPage
+  ],
+  exports: [
+
+  ]
 })
 export class ProductDetailsPageModule {}
