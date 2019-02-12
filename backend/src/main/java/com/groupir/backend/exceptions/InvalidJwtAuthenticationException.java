431a067649e7f7b226806d7f329c3ae39dc89f27
BaptisteWolff <baptiste.wolff@telecom-st-etienne.fr>
2019-02-12 11:58:22 +0100

    Get one user, get current user

--- backend/src/main/java/com/groupir/backend/exceptions/InvalidJwtAuthenticationException.java
+++ backend/src/main/java/com/groupir/backend/exceptions/InvalidJwtAuthenticationException.java
@@ -2,5 +2,6 @@ package com.groupir.backend.exceptions;
 
 public class InvalidJwtAuthenticationException extends RuntimeException {
     public InvalidJwtAuthenticationException(String expired_or_invalid_jwt_token) {
+        super(expired_or_invalid_jwt_token);
     }
 }
