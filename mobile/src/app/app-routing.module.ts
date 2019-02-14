7c8a4b9cc5e02e70cb04495b6f9ac3a0f0ada872
Kiupi <quent407@gmail.com>
2019-02-14 16:21:09 +0100

    [Frontend/login] protect main-supplier against access of simple user

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -5,12 +5,12 @@ import {AuthGuard} from './auth.guard';
 const routes: Routes = [
     {path: '', redirectTo: 'products', pathMatch: 'full'},
     {path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard]},
-    {path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule'},
+    {path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule', canActivate: [AuthGuard]},
     {path: 'login', loadChildren: './login/login.module#LoginPageModule'},
-    {path: 'forgot-page', loadChildren: './forgot-page/forgot-page.module#ForgotPagePageModule'},
+    {path: 'forgot-page', loadChildren: './forgot-page/forgot-page.module#ForgotPagePageModule', canActivate: [AuthGuard]},
     {path: 'products', loadChildren: './products/products.module#ProductsPageModule', canActivate: [AuthGuard]},
-    {path: 'product-details', loadChildren: './product-details/product-details.module#ProductDetailsPageModule' },
-    {path: 'main-supplier', loadChildren: './main-supplier/main-supplier.module#MainSupplierModule'},
+    {path: 'product-details', loadChildren: './product-details/product-details.module#ProductDetailsPageModule', canActivate: [AuthGuard] },
+    {path: 'main-supplier', loadChildren: './main-supplier/main-supplier.module#MainSupplierModule', canActivate: [AuthGuard]},
     {path: '**', redirectTo: 'login'}
 
 ];
--- mobile/src/app/main-supplier/main-supplier.page.ts
+++ mobile/src/app/main-supplier/main-supplier.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {OrderItem} from '../interface/orderItem.interface';
 import {SessionsServices} from '../sharedServices/sessions.services';
 import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-main-supplier',
@@ -14,32 +15,39 @@ export class MainSupplierPage implements OnInit {
     public orderItemsSent: Array<OrderItem>;
     public orderItemsNew: Array<OrderItem>;
     public new = true;
+    public canAccess;
 
-    constructor(private sessionsService: SessionsServices) {
+    constructor(private sessionsService: SessionsServices, private router: Router) {
         this.orderItemsNew = new Array<OrderItem>();
         this.orderItemsSent = new Array<OrderItem>();
     }
 
     ngOnInit() {
-        this.sessionsService.getOrderItems().subscribe(
-            (data: Array<OrderItem>) => {
-                console.log(data);
-                this.orderItems = data;
-                console.log(data);
-                this.orderItems.forEach((orderItem) => {
-                    console.log(orderItem);
-                    if (orderItem.dispatchmentDate !== null) {
-                        orderItem.dispatchmentDate= new Date(orderItem.dispatchmentDate*1000);
-                        console.log(orderItem.dispatchmentDate);
-                        this.orderItemsSent.push(orderItem);
-                    } else {
-                        this.orderItemsNew.push(orderItem);
-                    }
-                });
-            }
-        );
-    }
+        const user = JSON.parse(localStorage.getItem('user'));
 
+        if (user.role.roleName === 'SUPPLIER' || user.role.roleName === 'ADMIN') {
+            this.canAccess = true;
+            this.sessionsService.getOrderItems().subscribe(
+                (data: Array<OrderItem>) => {
+                    console.log(data);
+                    this.orderItems = data;
+                    console.log(data);
+                    this.orderItems.forEach((orderItem) => {
+                        console.log(orderItem);
+                        if (orderItem.dispatchmentDate !== null) {
+                            orderItem.dispatchmentDate = new Date(orderItem.dispatchmentDate * 1000);
+                            console.log(orderItem.dispatchmentDate);
+                            this.orderItemsSent.push(orderItem);
+                        } else {
+                            this.orderItemsNew.push(orderItem);
+                        }
+                    });
+                }
+            );
+        }else{
+            this.router.navigateByUrl('');
+        }
+    }
     validateSend(form: NgForm, orderItem: OrderItem) {
         this.orderItems.slice(this.orderItems.indexOf(orderItem));
         orderItem.dispatchmentDate = form.value['dispatchmentDate'];
@@ -63,12 +71,12 @@ export class MainSupplierPage implements OnInit {
 
 
     switchToNew() {
-        console.log("goToNew");
-        this.new=true;
+        console.log('goToNew');
+        this.new = true;
     }
 
     switchToSend() {
-        console.log("goToSend")
-        this.new=false;
+        console.log('goToSend');
+        this.new = false;
     }
 }
